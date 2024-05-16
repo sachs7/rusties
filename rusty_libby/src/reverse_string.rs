8693cf59d4d88e7d85e813dead6318d5cf3d2572
Sachin Uplaonkar <up.sachi@gmail.com>
2024-05-16 11:48:10 -0700

    Update reverse_string.rs

--- rusty_libby/src/reverse_string.rs
+++ rusty_libby/src/reverse_string.rs
@@ -1,3 +1,18 @@
+/// This module contains a struct that reverses a string
+/// 
+/// ReverseString struct has a field word of type String.
+/// ReverseString struct has a method new which takes a word and returns a ReverseString instance.
+/// ReverseString struct has a method reverse which returns a String value.
+/// ReverseString struct has a Display implementation which returns a formatted string.
+/// 
+/// Example:
+/// ```
+/// use rusty_libby::reverse_string::ReverseString;
+/// 
+/// let reverse_string = ReverseString::new("hello".to_string());
+/// assert_eq!(reverse_string.to_string(), "Reverse of hello is olleh");
+/// ```
+
 pub struct ReverseString {
     pub word: String,
 }
@@ -14,6 +29,6 @@ impl ReverseString {
 
 impl std::fmt::Display for ReverseString {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Reverse of {} is: {}", self.word, self.reverse())
+        write!(f, "Reverse of {} is {}", self.word, self.reverse())
     }
 }
